9d0fee860f78529af3ef6358cb945f2fe5212316
Abdelrahim_Aref <40636672+BEdoAref@users.noreply.github.com>
2019-04-18 21:32:52 +0300

    commit

--- js/home.js
+++ js/home.js
@@ -10,9 +10,7 @@ $(function(){
     $('#getStartForm').click(function(){
     $('.myCarousel').fadeOut(1200);
     $('.myFormBehindCarousel').slideDown(1000);    
-    });
-    
-    
+    });    
     $(window).on("scroll",function(){
         if($(window).scrollTop()>=300)
         {
@@ -73,6 +71,17 @@ $(function(){
         $('.whatsappConactM').fadeOut("slow");
     });
 });
+    /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "190px";
+  document.getElementById("main").style.marginLeft = "190px";
+  
+}
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
 function goUP(){
     window.scrollTo(0,0); 
 };
