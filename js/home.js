1b1a02bb5ff75c149688adca16aa2bbe42950525
Abdelrahim_Aref <40636672+BEdoAref@users.noreply.github.com>
2019-04-20 00:47:44 +0300

    commit

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
 //import Axios from "axios";
 "use strict";
 $(function(){
+
     $('#status').fadeOut(); // will first fade out the loading animation 
     $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
     $('body').delay(350).css({'overflow':'visible'});
